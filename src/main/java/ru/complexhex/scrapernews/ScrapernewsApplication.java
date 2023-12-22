4ad9979cbe36addc00f40b06147d37adbe9fbedb
Alex <lexx-81@yandex.ru>
2023-12-22 06:54:08 +0300

    v0.1

---
+++ src/main/java/ru/complexhex/scrapernews/ScrapernewsApplication.java
@@ -0,0 +1,13 @@
+package ru.complexhex.scrapernews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapernewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrapernewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/complexhex/scrapernews/ScrapernewsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.complexhex.scrapernews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapernewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
